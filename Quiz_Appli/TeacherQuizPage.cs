8523d746b7420e9c7f3f133c47c2f12c2dc02039
ck15-code <luigeeespiritu1505@gmail.com>
2025-05-07 16:49:12 +0800

    geh

--- Quiz_Appli/TeacherQuizPage.cs
+++ Quiz_Appli/TeacherQuizPage.cs
@@ -171,7 +171,7 @@ namespace Quiz_Appli
 
         private void btnHome_Click(object sender, EventArgs e)
         {
-            frmTeacherHomepage frm = new frmTeacherHomepage();
+            frmTeacherHomepage frm = new frmTeacherHomepage(AppContext.CurrentTeacherId);
             frm.Show();
 
             if (this.WindowState == FormWindowState.Maximized)
@@ -224,7 +224,7 @@ namespace Quiz_Appli
 
         private void btnProfile_Click(object sender, EventArgs e)
         {
-            frmTeacherProfile frm = new frmTeacherProfile();
+            frmTeacherProfile frm = new frmTeacherProfile(AppContext.CurrentTeacherId);
             frm.Show();
 
             if (this.WindowState == FormWindowState.Maximized)
